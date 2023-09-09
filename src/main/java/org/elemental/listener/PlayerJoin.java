a86e2e24492c69485cd7f1e08b42fb1653949a3f
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-09-09 11:52:14 -0400

    default examples

---
+++ src/main/java/org/elemental/listener/PlayerJoin.java
@@ -0,0 +1,17 @@
+package org.elemental.listener;
+
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerJoin implements Listener {
+
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        player.sendMessage(ChatColor.RED + "" + ChatColor.BOLD + "This game is under development so things may not work!");
+    }
+}
---
+++ src/main/java/org/elemental/variables/Locations.java
@@ -0,0 +1,9 @@
+package org.elemental.variables;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+
+public class Locations {
+    public static Location lobbyLoc = new Location(Bukkit.getWorld("world"), 41, 18, -119);
+    public static Location jail = new Location(Bukkit.getWorld("world"), -13, 67, 1);
+}
