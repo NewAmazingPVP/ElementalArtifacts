e960547305f43b6e3b5c3b63a108c612791d19b4
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-10 13:55:28 -0400

    Update ElementalArtifactsGuide_1.java

--- src/main/java/org/elemental/listeners/ElementalArtifactsGuide_1.java
+++ src/main/java/org/elemental/listeners/ElementalArtifactsGuide_1.java
@@ -1,11 +1,14 @@
 package org.elemental.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -19,7 +22,22 @@ public class ElementalArtifactsGuide_1 implements Listener {
             if (item != null && item.hasItemMeta()) {
                 ItemMeta meta = item.getItemMeta();
                 if (meta.hasDisplayName() && meta.getDisplayName().equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Elemental Artifacts Guide")) {
-                    player.sendMessage(ChatColor.GREEN + "Test successful!");
+                    Player p = event.getPlayer();
+                    player.playSound(player.getLocation(), "minecraft:block.enchantment_table.use", 1.0f, 2.0f);
+                    Inventory Guide = Bukkit.createInventory(p, 27, ChatColor.GOLD + "" + ChatColor.BOLD + "Elemental Artifacts Guide");
+
+
+
+
+                    ItemStack rteam = new ItemStack(Material.RED_CONCRETE);
+                    ItemMeta RMeta = rteam.getItemMeta();
+                    RMeta.setDisplayName(ChatColor.YELLOW + "" + ChatColor.BOLD + "Click" + ChatColor.DARK_RED + " To Join Red");
+                    rteam.setItemMeta(RMeta);
+                    Guide.setItem(11, rteam);
+
+
+
+                    p.openInventory(Guide);
                 }
             }
         }
