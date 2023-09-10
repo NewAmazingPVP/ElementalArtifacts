cda459fa916bf5a0c20ac23ec6f3f5f50935eef5
NewAmazingPVP <gamingofpat@gmail.com>
2023-09-09 21:09:07 -0400

    Update Artifacts.java

--- src/main/java/org/elemental/command/Artifacts.java
+++ src/main/java/org/elemental/command/Artifacts.java
@@ -15,24 +15,21 @@ public class Artifacts implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("givebook")) {
-            if (args.length == 1) {
-                Player player = Bukkit.getPlayer(args[0]);
-                if (player != null) {
-                    ItemStack book = createEnchantedBook();
-                    player.getInventory().addItem(book);
-                    player.sendMessage("You have received the Elemental Artifacts Recipe Book.");
-                    return true;
-                } else {
-                    sender.sendMessage("Player not found.");
-                    return false;
-                }
+        if (args.length == 1) {
+            Player player = Bukkit.getPlayer(args[0]);
+            if (player != null) {
+                ItemStack book = createEnchantedBook();
+                player.getInventory().addItem(book);
+                player.sendMessage("You have received the Elemental Artifacts Recipe Book.");
+                return true;
             } else {
-                sender.sendMessage("Usage: /givebook <player>");
+                sender.sendMessage("Player not found.");
                 return false;
             }
+        } else {
+            sender.sendMessage("Usage: /givebook <player>");
+            return false;
         }
-        return false;
     }
 
     private ItemStack createEnchantedBook() {
