938902f1c0e2f3f9392ba38a3e38faf2f62af46f
NewAmazingPVP <gamingofpat@gmail.com>
2023-09-10 11:50:53 -0400

    test

--- src/main/java/org/elemental/ElementalArtifacts.java
+++ src/main/java/org/elemental/ElementalArtifacts.java
@@ -28,7 +28,7 @@ public final class ElementalArtifacts extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-        Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "Elemental Artifacts Has Stopped !!!");
+        Bukkit.broadcastMessage(ChatColor.RED + "" + ChatColor.BOLD + "Elemental Artifacts Has Stoppeds !!!");
         // Plugin shutdown logic
     }
 }
