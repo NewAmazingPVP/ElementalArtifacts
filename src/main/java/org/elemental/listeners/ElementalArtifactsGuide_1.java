0154745e776024f50f1f8206239830c399ab7ba1
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-10 18:04:55 -0400

    1

--- src/main/java/org/elemental/listeners/ElementalArtifactsGuide_1.java
+++ src/main/java/org/elemental/listeners/ElementalArtifactsGuide_1.java
@@ -3,12 +3,14 @@ package org.elemental.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -32,6 +34,31 @@ public class ElementalArtifactsGuide_1 implements Listener {
                     M1.setDisplayName(" ");
                     G1.setItemMeta(M1);
 
+
+                    ItemStack Class = new ItemStack(Material.DIAMOND_SWORD);
+                    ItemMeta Mclass = Class.getItemMeta();
+                    Mclass.setDisplayName(" ");
+                    Class.setItemMeta(Mclass);
+
+                    ItemStack R1 = new ItemStack(Material.CRAFTING_TABLE);
+                    ItemMeta MR1 = R1.getItemMeta();
+                    MR1.setDisplayName(" ");
+                    R1.setItemMeta(MR1);
+
+                    ItemStack Info = new ItemStack(Material.PAPER);
+                    ItemMeta Minfo = Info.getItemMeta();
+                    Minfo.setDisplayName(" ");
+                    Minfo.addEnchant(Enchantment.DURABILITY, 1, false);
+                    Minfo.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                    Info.setItemMeta(Minfo);
+
+
+
+
+
+
+
+
                     Guide.setItem(0, G1);
                     Guide.setItem(1, G1);
                     Guide.setItem(2, G1);
@@ -42,6 +69,24 @@ public class ElementalArtifactsGuide_1 implements Listener {
                     Guide.setItem(7, G1);
                     Guide.setItem(8, G1);
                     Guide.setItem(9, G1);
+                    Guide.setItem(10, G1);
+                    Guide.setItem(11, Class);
+                    Guide.setItem(12, G1);
+                    Guide.setItem(13, R1);
+                    Guide.setItem(14, G1);
+                    Guide.setItem(15, Info);
+                    Guide.setItem(16, G1);
+                    Guide.setItem(17, G1);
+                    Guide.setItem(18, G1);
+                    Guide.setItem(19, G1);
+                    Guide.setItem(20, G1);
+                    Guide.setItem(21, G1);
+                    Guide.setItem(22, G1);
+                    Guide.setItem(23, G1);
+                    Guide.setItem(24, G1);
+                    Guide.setItem(25, G1);
+                    Guide.setItem(26, G1);
+                    Guide.setItem(27, G1);
 
 
 
