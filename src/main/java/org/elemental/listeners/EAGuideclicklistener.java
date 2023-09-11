fed2bc18979be4cc5758e58ee5cfb59e22d4724d
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-10 20:42:23 -0400

    1

--- src/main/java/org/elemental/listeners/EAGuideclicklistener.java
+++ src/main/java/org/elemental/listeners/EAGuideclicklistener.java
@@ -5,6 +5,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 
@@ -12,14 +13,20 @@ public class EAGuideclicklistener implements Listener {
 
     @EventHandler
     public void onMenuClick(InventoryClickEvent event) {
-        //this is for the team select menu thing good luck de bugging xD
         if (event.getView().getTitle().equalsIgnoreCase(ChatColor.GOLD + "" + ChatColor.BOLD + "Elemental Artifacts Guide")) {
 
 
             if (event.getCurrentItem() == null) {
                 return;
             }
-            event.setCancelled(true);
+
+            if (event.getCurrentItem().getType() == Material.CRAFTING_TABLE) {
+                Player player = (Player) event.getView().getPlayer();
+                player.playSound(player.getLocation(), "minecraft:block.enchantment_table.use", 1.0f, 2.0f);
+                Inventory Guide = Bukkit.createInventory(player, 36, ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "Crafting Recipes");
+
+            }
         }
+        event.setCancelled(true);
     }
 }
