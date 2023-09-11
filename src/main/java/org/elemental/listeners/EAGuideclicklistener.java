4966685cc4ebc22613c858a5cc0e2f3800ce03e3
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-10 21:32:20 -0400

    1

--- src/main/java/org/elemental/listeners/EAGuideclicklistener.java
+++ src/main/java/org/elemental/listeners/EAGuideclicklistener.java
@@ -26,7 +26,7 @@ public class EAGuideclicklistener implements Listener {
             if (event.getCurrentItem().getType() == Material.CRAFTING_TABLE) {
                 Player player = (Player) event.getView().getPlayer();
                 player.playSound(player.getLocation(), "minecraft:block.enchantment_table.use", 1.0f, 2.0f);
-                Inventory Guide = Bukkit.createInventory(player, 27, ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "Crafting Recipes");
+                Inventory Guide = Bukkit.createInventory(player, 36, ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + "Crafting Recipes");
 
                 ItemStack G1 = new ItemStack(Material.ORANGE_STAINED_GLASS_PANE);
                 ItemMeta M1 = G1.getItemMeta();
@@ -40,7 +40,6 @@ public class EAGuideclicklistener implements Listener {
                 WM.addItemFlags(ItemFlag.HIDE_ENCHANTS);
                 weapons.setItemMeta(WM);
 
-
                 ItemStack mat = new ItemStack(Material.NETHERITE_INGOT);
                 ItemMeta MM = mat.getItemMeta();
                 MM.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "materials");
@@ -51,19 +50,59 @@ public class EAGuideclicklistener implements Listener {
                 MI.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "miscellaneous");
                 mis.setItemMeta(MI);
 
-                ItemStack armor = new ItemStack(Material.DIRT);
+                ItemStack armor = new ItemStack(Material.DIAMOND_CHESTPLATE);
                 ItemMeta AMI = armor.getItemMeta();
                 AMI.setDisplayName(ChatColor.GREEN + "" + ChatColor.BOLD + "Armor");
                 armor.setItemMeta(AMI);
 
-                /*LeatherArmorMeta armorMeta = (LeatherArmorMeta) armor.getItemMeta();
-                armorMeta.setColor(Color.fromRGB(128, 0, 0)); // Set the color to dark red
-                armorMeta.setDisplayName(ChatColor.GREEN + "" + ChatColor.BOLD + "Armor");
-                armor.setItemMeta(armorMeta);*/
+                ItemStack back = new ItemStack(Material.ARROW);
+                ItemMeta BM = back.getItemMeta();
+                BM.setDisplayName(ChatColor.RED + "Back");
+                back.setItemMeta(BM);
+
+                ItemStack close = new ItemStack(Material.BARRIER);
+                ItemMeta CM = close.getItemMeta();
+                CM.setDisplayName(ChatColor.GREEN + "" + ChatColor.BOLD + "Close");
+                close.setItemMeta(CM);
 
 
-                Guide.setItem(0, weapons);
 
+                Guide.setItem(0, G1);
+                Guide.setItem(1, G1);
+                Guide.setItem(2, G1);
+                Guide.setItem(3, G1);
+                Guide.setItem(4, G1);
+                Guide.setItem(5, G1);
+                Guide.setItem(6, G1);
+                Guide.setItem(7, G1);
+                Guide.setItem(8, G1);
+                Guide.setItem(9, G1);
+                Guide.setItem(10, weapons);
+                Guide.setItem(11, G1);
+                Guide.setItem(12, mat);
+                Guide.setItem(13, G1);
+                Guide.setItem(14, armor);
+                Guide.setItem(15, G1);
+                Guide.setItem(16, mis);
+                Guide.setItem(17, G1);
+                Guide.setItem(18, G1);
+                Guide.setItem(19, G1);
+                Guide.setItem(20, G1);
+                Guide.setItem(21, G1);
+                Guide.setItem(22, G1);
+                Guide.setItem(23, G1);
+                Guide.setItem(24, G1);
+                Guide.setItem(25, G1);
+                Guide.setItem(26, G1);
+                Guide.setItem(17, back);
+                Guide.setItem(28, G1);
+                Guide.setItem(29, G1);
+                Guide.setItem(30, G1);
+                Guide.setItem(31, G1);
+                Guide.setItem(32, G1);
+                Guide.setItem(33, G1);
+                Guide.setItem(34, G1);
+                Guide.setItem(35, close);
 
 
 
