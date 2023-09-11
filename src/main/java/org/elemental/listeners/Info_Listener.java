f398b7ae55238c76043b28479e288470a53bc1b6
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-11 18:56:51 -0400

    1

---
+++ src/main/java/org/elemental/listeners/Info_Listener.java
@@ -0,0 +1,38 @@
+package org.elemental.listeners;
+
+import org.bukkit.*;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Info_Listener implements Listener {
+
+    @EventHandler
+    public void onMenuClick(InventoryClickEvent event) {
+        if (event.getView().getTitle().equalsIgnoreCase(ChatColor.BLUE + "" + ChatColor.BOLD + "Info")) {
+
+
+            if (event.getCurrentItem() == null) {
+                return;
+            }
+
+            if (event.getCurrentItem().getType() == Material.BARRIER) {
+                Player player = (Player) event.getView().getPlayer();
+                player.playSound(player.getLocation(), "minecraft:block.enchantment_table.use", 1.0f, 2.0f);
+                player.closeInventory();
+
+            }
+            event.setCancelled(true);
+        }
+
+    }
+}
