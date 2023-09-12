70ddec6e32a6335a86359b88d6a8dedfb5f54004
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-11 21:00:13 -0400

    1

--- src/main/java/org/elemental/listeners/Info_Listener.java
+++ src/main/java/org/elemental/listeners/Info_Listener.java
@@ -31,8 +31,76 @@ public class Info_Listener implements Listener {
                 player.closeInventory();
 
             }
+            if (event.getCurrentItem().getType() == Material.ARROW) {
+                Player player = (Player) event.getView().getPlayer();
+                player.playSound(player.getLocation(), "minecraft:block.enchantment_table.use", 1.0f, 2.0f);
+                Inventory Guide = Bukkit.createInventory(player, 27, ChatColor.GOLD + "" + ChatColor.BOLD + "Elemental Artifacts Guide");
+
+
+                ItemStack G1 = new ItemStack(Material.PURPLE_STAINED_GLASS_PANE);
+                ItemMeta M1 = G1.getItemMeta();
+                M1.setDisplayName(" ");
+                G1.setItemMeta(M1);
+
+
+                ItemStack Class = new ItemStack(Material.END_CRYSTAL);
+                ItemMeta Mclass = Class.getItemMeta();
+                Mclass.setDisplayName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Class");
+                Class.setItemMeta(Mclass);
+
+                ItemStack R1 = new ItemStack(Material.CRAFTING_TABLE);
+                ItemMeta MR1 = R1.getItemMeta();
+                MR1.setDisplayName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Crafting Recipes");
+                R1.setItemMeta(MR1);
+
+                ItemStack Info = new ItemStack(Material.PAPER);
+                ItemMeta Minfo = Info.getItemMeta();
+                Minfo.setDisplayName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Info");
+                Minfo.addEnchant(Enchantment.DURABILITY, 1, false);
+                Minfo.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+                Info.setItemMeta(Minfo);
+
+                ItemStack Skill = new ItemStack(Material.BEACON);
+                ItemMeta MS = Skill.getItemMeta();
+                MS.setDisplayName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Skill Points");
+                Skill.setItemMeta(MS);
+
+
+                Guide.setItem(0, G1);
+                Guide.setItem(1, G1);
+                Guide.setItem(2, G1);
+                Guide.setItem(3, G1);
+                Guide.setItem(4, G1);
+                Guide.setItem(5, G1);
+                Guide.setItem(6, G1);
+                Guide.setItem(7, G1);
+                Guide.setItem(8, Info);
+                Guide.setItem(9, G1);
+                Guide.setItem(10, G1);
+                Guide.setItem(11, Class);
+                Guide.setItem(12, G1);
+                Guide.setItem(13, R1);
+                Guide.setItem(14, G1);
+                Guide.setItem(15, Skill);
+                Guide.setItem(16, G1);
+                Guide.setItem(17, G1);
+                Guide.setItem(18, G1);
+                Guide.setItem(19, G1);
+                Guide.setItem(20, G1);
+                Guide.setItem(21, G1);
+                Guide.setItem(22, G1);
+                Guide.setItem(23, G1);
+                Guide.setItem(24, G1);
+                Guide.setItem(25, G1);
+                Guide.setItem(26, G1);
+
+
+                player.openInventory(Guide);
+
+
+
+            }
             event.setCancelled(true);
         }
-
     }
 }
