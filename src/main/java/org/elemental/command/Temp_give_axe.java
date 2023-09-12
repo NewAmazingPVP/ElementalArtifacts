541d1b492a7cc076131ef373495db7d76c0f7077
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-12 18:01:54 -0400

    1

--- src/main/java/org/elemental/command/Temp_give_axe.java
+++ src/main/java/org/elemental/command/Temp_give_axe.java
@@ -29,7 +29,9 @@ public class Temp_give_axe implements CommandExecutor {
         List<String> DEFL = new ArrayList<>();
         DEFL.add(ChatColor.RED + "Use this to show your power and execute people");
         DEFL.add(ChatColor.RED + "to keep there heads or something idk...");
+        DEFL.add(ChatColor.DARK_RED + "lol...");
         SI.setLore(DEFL);
+        SI.setUnbreakable(true);
         teamsel.setItemMeta(SI);
 
         player.getInventory().addItem(teamsel);
