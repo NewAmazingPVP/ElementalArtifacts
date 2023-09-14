4e67d6b9a8db8a383501e4ae5bdfe235a7b62667
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-14 17:29:07 -0400

    test

---
+++ src/main/java/org/elemental/CustomRecipes/Compact_Netherite_Recipe.java
@@ -0,0 +1,26 @@
+package org.elemental.CustomRecipes;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.plugin.Plugin;
+
+public class Compact_Netherite_Recipe {
+
+
+    private void registerCustomRecipes() {
+        ItemStack shulker = new ItemStack(Material.SHULKER_BOX);
+        ShapedRecipe shulkerRecipe = new ShapedRecipe(new NamespacedKey((Plugin) this, "shulker_recipe"), shulker);
+        shulkerRecipe.shape("DDD", "DCD", "DDD");
+        shulkerRecipe.setIngredient('C', Material.CHEST);
+        shulkerRecipe.setIngredient('D', Material.DIAMOND);
+        Bukkit.addRecipe(shulkerRecipe);
+
+    }
+}
