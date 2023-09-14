06587876c71bcd4ae52cd8d48a282db6c29eb9df
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-14 17:05:11 -0400

    1

--- src/main/java/org/elemental/listeners/doubleedged_sword.java
+++ src/main/java/org/elemental/listeners/doubleedged_sword.java
@@ -1,27 +1,24 @@
 package org.elemental.listeners;
 
-import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.Material;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class doubleedged_sword implements Listener {
-
 
     @EventHandler
     public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
         if (event.getDamager() instanceof Player) {
             Player player = (Player) event.getDamager();
-            ItemStack item = player.getInventory().getItemInMainHand();
-            if (item.getType() == Material.DIAMOND_SWORD && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
-                    && item.getItemMeta().getDisplayName().equals(ChatColor.WHITE + "" + ChatColor.BOLD + "Double-Edged Sword" + ChatColor.DARK_AQUA + " [Sword]")) {
-                player.playSound(player.getLocation(), "minecraft:block.glass.break", 1.0f, 2.0f);
-                player.damage(7.0);
+            if (player.getInventory().getItemInMainHand().getItemMeta().getDisplayName().equals(ChatColor.WHITE + "" + ChatColor.BOLD + "Double-Edged Sword" + ChatColor.DARK_AQUA + " [Sword]")) {
+                // Check if the entity hit is an instance of an entity
+                if (event.getEntity() instanceof Entity) {
+                    // Apply 5 damage to the hit entity
+                    event.setDamage(5);
+                }
             }
         }
     }
