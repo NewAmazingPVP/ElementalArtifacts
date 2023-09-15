ef161b1ecb7143689add4bea041fe4d0610a0daa
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-14 21:55:40 -0400

    1

--- src/main/java/org/elemental/CustomRecipes/Compact_Netherite_Block_Recipe.java
+++ src/main/java/org/elemental/CustomRecipes/Compact_Netherite_Block_Recipe.java
@@ -10,6 +10,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.RecipeChoice;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
@@ -27,14 +28,22 @@ public class Compact_Netherite_Block_Recipe {
 
 
         ItemMeta SI = compact.getItemMeta();
-        SI.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Compact Netherite Block");
+        SI.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Compact Netherite Ingot");
         SI.addEnchant(Enchantment.DURABILITY, 1, false);
         SI.addItemFlags(ItemFlag.HIDE_ENCHANTS);
         compact.setItemMeta(SI);
 
-        ShapedRecipe shulkerRecipe = new ShapedRecipe(new NamespacedKey(elementalArtifacts, "test"), compact);
+
+        ItemStack compact2 = new ItemStack(Material.NETHERITE_BLOCK);
+        ItemMeta SI2 = compact2.getItemMeta();
+        SI2.setDisplayName(ChatColor.GOLD + "" + ChatColor.BOLD + "Compact Netherite Block");
+        SI2.addEnchant(Enchantment.DURABILITY, 1, false);
+        SI2.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+        compact2.setItemMeta(SI2);
+
+        ShapedRecipe shulkerRecipe = new ShapedRecipe(new NamespacedKey(elementalArtifacts, "test"), compact2);
         shulkerRecipe.shape("DDD", "DDD", "DDD");
-        shulkerRecipe.setIngredient('D', Material.NETHERITE_INGOT);
+        shulkerRecipe.setIngredient('D', new RecipeChoice.ExactChoice(compact));
         Bukkit.addRecipe(shulkerRecipe);
 
     }
