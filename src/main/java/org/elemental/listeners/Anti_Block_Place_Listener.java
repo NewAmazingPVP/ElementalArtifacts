76faded5a56d5d375d050ed7b330f56ff07363ad
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-16 18:53:52 -0400

    1

--- src/main/java/org/elemental/listeners/Anti_Block_Place_Listener.java
+++ src/main/java/org/elemental/listeners/Anti_Block_Place_Listener.java
@@ -16,5 +16,8 @@ public class Anti_Block_Place_Listener implements Listener {
         if (meta != null && meta.hasDisplayName() && meta.getDisplayName().equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Compact Netherite Block")) {
             event.setCancelled(true);
         }
+        if (meta != null && meta.hasDisplayName() && meta.getDisplayName().equals(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "God Potion" + ChatColor.DARK_AQUA + " [Potion]")) {
+            event.setCancelled(true);
+        }
     }
 }
