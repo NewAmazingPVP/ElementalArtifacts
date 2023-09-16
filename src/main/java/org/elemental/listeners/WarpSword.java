bda48c023ac79629327042d7ebf3b5415835d914
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-16 08:29:12 -0400

    1

--- src/main/java/org/elemental/listeners/WarpSword.java
+++ src/main/java/org/elemental/listeners/WarpSword.java
@@ -29,9 +29,9 @@ public class WarpSword implements Listener {
                         return;
                     }
                 }
-                cooldowns.put(player.getUniqueId(), System.currentTimeMillis() + (5 * 1000));
+                cooldowns.put(player.getUniqueId(), System.currentTimeMillis() + (2 * 1000));
 
-                Vector direction = player.getLocation().getDirection().multiply(5);
+                Vector direction = player.getLocation().getDirection().multiply(15);
                 Location destination = player.getLocation().add(direction);
                 Block feet = destination.getBlock();
                 Block chest = feet.getRelative(BlockFace.UP);
