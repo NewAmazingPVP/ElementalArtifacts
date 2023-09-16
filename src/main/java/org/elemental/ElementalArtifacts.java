e40ea21485cede5e14af2c16bd0a16afca35c236
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-16 17:56:11 -0400

    test

--- src/main/java/org/elemental/ElementalArtifacts.java
+++ src/main/java/org/elemental/ElementalArtifacts.java
@@ -47,7 +47,7 @@ public final class ElementalArtifacts extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(new WarpSword(), this);
         getServer().getPluginManager().registerEvents(new Book_Of_Items(), this);
         getServer().getPluginManager().registerEvents(new Book_Of_Items_Listener(), this);
-        getServer().getPluginManager().registerEvents(new Anti_Item_Thrower_Listener(), this);
+        /*getServer().getPluginManager().registerEvents(new Anti_Item_Thrower_Listener(), this);*/
         registerCustomRecipes();
         registerCustom();
         registerCustomRecipesdouble();
