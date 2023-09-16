d0c23fbb684641cf6cccdb5e2dc799e543d6816c
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-16 08:52:13 -0400

    1

---
+++ src/main/java/org/elemental/listeners/Book_Of_Items.java
@@ -0,0 +1,36 @@
+package org.elemental.listeners;
+
+import org.bukkit.*;
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
+public class Book_Of_Items implements Listener {
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        Player player = event.getPlayer();
+        if (event.getAction().equals(Action.RIGHT_CLICK_AIR) || event.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            ItemStack item = event.getItem();
+            if (item != null && item.hasItemMeta()) {
+                ItemMeta meta = item.getItemMeta();
+                if (meta.hasDisplayName() && meta.getDisplayName().equals(ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Book Of Items")) {
+                    Player p = event.getPlayer();
+                    player.playSound(player.getLocation(), "minecraft:block.enchantment_table.use", 1.0f, 2.0f);
+                    player.getWorld().spawnParticle(Particle.REDSTONE, player.getLocation(), 10, 0.6, 0.6, 0.6, new Particle.DustOptions(Color.ORANGE, 3));
+                    Inventory Guide = Bukkit.createInventory(p, 27, ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Item Menu");
+
+
+                    p.openInventory(Guide);
+                }
+            }
+        }
+    }
+}
