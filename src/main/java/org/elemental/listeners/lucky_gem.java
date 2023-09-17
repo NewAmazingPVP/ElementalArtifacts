cc5f0a5f303f90643e8cd0f81412ea8ae11f94e2
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-17 10:58:10 -0400

    1

--- src/main/java/org/elemental/listeners/lucky_gem.java
+++ src/main/java/org/elemental/listeners/lucky_gem.java
@@ -2,6 +2,7 @@ package org.elemental.listeners;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -9,10 +10,14 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+
+import static org.bukkit.Bukkit.getServer;
 
 public class lucky_gem implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
+        getServer().getPluginManager().registerEvents((Listener) this, (Plugin) this);
         Player player = event.getPlayer();
         ItemStack item = player.getInventory().getItemInMainHand();
 
@@ -30,18 +35,41 @@ public class lucky_gem implements Listener {
                     player.getInventory().setChestplate(new ItemStack(Material.DIAMOND_CHESTPLATE));
                     player.getInventory().setLeggings(new ItemStack(Material.DIAMOND_LEGGINGS));
                     player.getInventory().setBoots(new ItemStack(Material.DIAMOND_BOOTS));
+                    player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1.0f, 1.0f);
+                    player.playSound(player.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, 1.0f, 1.0f);
+                    String titleText = ChatColor.AQUA + "" + ChatColor.BOLD + "+ Diamond Set";
+                    int fadeIn = 10;
+                    int stay = 40;
+                    int fadeOut = 10;
+
                 } else if (random <= 0.15) {
                     // Give a diamond block
                     player.getInventory().addItem(new ItemStack(Material.DIAMOND_BLOCK));
+                    player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1.0f, 1.0f);
+                    String titleText = ChatColor.AQUA + "" + ChatColor.BOLD + "+ Diamond Block";
+                    int fadeIn = 10;
+                    int stay = 40;
+                    int fadeOut = 10;
                 } else if (random <= 0.25) {
                     // Give a god potion
                     player.getInventory().addItem(new ItemStack(Material.ENCHANTED_GOLDEN_APPLE));
+                    player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 1.0f, 1.0f);
+                    String titleText = ChatColor.GOLD + "" + ChatColor.BOLD + "+ God Apple";
+                    int fadeIn = 10;
+                    int stay = 40;
+                    int fadeOut = 10;
                 } else {
                     // Give leather armor
                     player.getInventory().setHelmet(new ItemStack(Material.LEATHER_HELMET));
                     player.getInventory().setChestplate(new ItemStack(Material.LEATHER_CHESTPLATE));
                     player.getInventory().setLeggings(new ItemStack(Material.LEATHER_LEGGINGS));
                     player.getInventory().setBoots(new ItemStack(Material.LEATHER_BOOTS));
+                    player.playSound(player.getLocation(), Sound.ENTITY_CAT_AMBIENT, 1.0f, 0.0f);
+                    String titleText = ChatColor.DARK_RED + "" + ChatColor.BOLD + "+ Binding Armor";
+                    String subtitleText = ChatColor.DARK_RED + "L              =D";
+                    int fadeIn = 10;
+                    int stay = 40;
+                    int fadeOut = 10;
                 }
             }
         }
