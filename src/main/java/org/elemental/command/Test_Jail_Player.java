9ce4c06fcbcfa1560d878a116a4f2b9ec2641579
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-20 17:19:22 -0400

    1

--- src/main/java/org/elemental/command/Test_Jail_Player.java
+++ src/main/java/org/elemental/command/Test_Jail_Player.java
@@ -3,10 +3,14 @@ package org.elemental.command;
 import net.milkbowl.vault.chat.Chat;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
+import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.ScoreboardManager;
 import org.bukkit.scoreboard.Team;
@@ -43,6 +47,35 @@ public class Test_Jail_Player implements CommandExecutor {
         team.addEntry(player.getName());
 
         sender.sendMessage(ChatColor.GOLD + playerName + " is now a prisoner !.");
-        return true;
+
+        if (player.getScoreboard().getEntryTeam(player.getName()).getName().equalsIgnoreCase("prisoner")) {
+
+
+            ItemStack chestplate = new ItemStack(Material.LEATHER_CHESTPLATE);
+            LeatherArmorMeta chestplateMeta = (LeatherArmorMeta) chestplate.getItemMeta();
+            Color color = Color.fromRGB(255, 0, 0); // Red color
+            chestplateMeta.setUnbreakable(true);
+            chestplate.setItemMeta(chestplateMeta);
+
+            ItemStack leggings = new ItemStack(Material.LEATHER_LEGGINGS);
+            LeatherArmorMeta leggingsMeta = (LeatherArmorMeta) leggings.getItemMeta();
+            leggingsMeta.setColor(Color.ORANGE);
+            leggingsMeta.setUnbreakable(true);
+            leggings.setItemMeta(leggingsMeta);
+
+            ItemStack boots = new ItemStack(Material.LEATHER_BOOTS);
+            LeatherArmorMeta bootsMeta = (LeatherArmorMeta) boots.getItemMeta();
+            bootsMeta.setColor(Color.ORANGE);
+            bootsMeta.setUnbreakable(true);
+            boots.setItemMeta(bootsMeta);
+
+
+            player.getInventory().setHelmet(null);
+            player.getInventory().setChestplate(chestplate);
+            player.getInventory().setLeggings(leggings);
+            player.getInventory().setBoots(boots);
+            return true;
+        }
+        return false;
     }
 }
