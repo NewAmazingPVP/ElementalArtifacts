658a94de5696aec843b6d4f945d862d0158802fa
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-22 16:20:13 -0400

    1

--- src/main/java/org/elemental/listeners/ClassMenu_Listener.java
+++ src/main/java/org/elemental/listeners/ClassMenu_Listener.java
@@ -31,7 +31,7 @@ public class ClassMenu_Listener implements Listener {
                 player.closeInventory();
 
             }
-            if (event.getCurrentItem().getDisplayName().equalsIgnoreCase("Close") && event.getCurrentItem().getType() == Material.ARROW) {
+            if (event.getCurrentItem().getDisplayName().equalsIgnoreCase(ChatColor.RED + "Back") && event.getCurrentItem().getType() == Material.ARROW) {
                 Player player = (Player) event.getView().getPlayer();
                 player.playSound(player.getLocation(), "minecraft:block.enchantment_table.use", 1.0f, 2.0f);
                 Inventory Guide = Bukkit.createInventory(player, 27, ChatColor.GOLD + "" + ChatColor.BOLD + "Elemental Artifacts Guide");
