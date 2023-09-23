850ee3f0c54dae0dcdce33040a430c72a86f1751
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-22 23:36:26 -0400

    1

--- src/main/java/org/elemental/listeners/Materials_Recipes_Listener.java
+++ src/main/java/org/elemental/listeners/Materials_Recipes_Listener.java
@@ -1,4 +1,18 @@
 package org.elemental.listeners;
 
+import org.bukkit.ChatColor;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
 public class Materials_Recipes_Listener {
+
+    public void onMenuClick(InventoryClickEvent event) {
+        if (event.getView().getTitle().equalsIgnoreCase(ChatColor.GOLD + "" + ChatColor.BOLD + "Materials Recipes")) {
+
+
+            if (event.getCurrentItem() == null) {
+                return;
+            }
+            event.setCancelled(true);
+        }
+    }
 }
