212f1aebd8d7840dfc6663a8c842fee7776c8ab5
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-23 16:56:28 -0400

    1

--- src/main/java/org/elemental/listeners/Gold_Covered_Meat.java
+++ src/main/java/org/elemental/listeners/Gold_Covered_Meat.java
@@ -2,12 +2,15 @@ package org.elemental.listeners;
 
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Gold_Covered_Meat implements Listener {
 
@@ -19,7 +22,9 @@ public class Gold_Covered_Meat implements Listener {
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
             if (item.getType() == Material.COOKED_MUTTON && item.hasItemMeta() && item.getItemMeta().hasDisplayName()
                     && item.getItemMeta().getDisplayName().equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Gold Covered Meat" + ChatColor.DARK_AQUA + " [Food]")) {
-                player.sendMessage("testmain");
+                player.playSound(player.getLocation(), Sound.ENTITY_GENERIC_EAT, 1.0f, 1.0f);
+                player.setSaturation(10);
+                player.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 20 * 60 * 10, 5));
                 event.setCancelled(true);
                 if (item.getAmount() > 1) {
                     item.setAmount(item.getAmount() - 1);
@@ -35,7 +40,9 @@ public class Gold_Covered_Meat implements Listener {
                 if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
                     if (offHandItem.getType() == Material.COOKED_MUTTON && offHandItem.hasItemMeta() && offHandItem.getItemMeta().hasDisplayName()
                             && offHandItem.getItemMeta().getDisplayName().equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Gold Covered Meat" + ChatColor.DARK_AQUA + " [Food]")) {
-                        player.sendMessage("testoff");
+                        player.playSound(player.getLocation(), Sound.ENTITY_GENERIC_EAT, 1.0f, 1.0f);
+                        player.setSaturation(10);
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.ABSORPTION, 20 * 60 * 10, 5));
                         event.setCancelled(true);
                         if (offHandItem.getAmount() > 1) {
                             offHandItem.setAmount(offHandItem.getAmount() - 1);
