80c4a2b35bc54c7c8ab64f7e05877feb36934698
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-23 13:40:31 -0400

    1

---
+++ src/main/java/org/elemental/listeners/Gold_Covered_Meat.java
@@ -0,0 +1,39 @@
+package org.elemental.listeners;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.entity.Player;
+
+public class Gold_Covered_Meat implements Listener {
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if (event.getAction().name().contains("RIGHT")) {  // Check if right-click
+            Player player = event.getPlayer();
+            ItemStack item = event.getItem();
+            if (item != null && item.getType() != Material.COOKED_MUTTON) {
+                ItemMeta meta = item.getItemMeta();
+                if (meta != null && meta.hasDisplayName() && meta.getDisplayName().equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Gold Covered Meat" + ChatColor.DARK_AQUA + " [Food]")) {
+                    ItemStack[] inventory = player.getInventory().getContents();
+                    for (int i = 0; i < inventory.length; i++) {
+                        ItemStack stack = inventory[i];
+                        if (stack != null && stack.isSimilar(item)) {
+                            int amount = stack.getAmount();
+                            if (amount > 1) {
+                                stack.setAmount(amount - 1);
+                            } else {
+                                player.getInventory().setItem(i, null);
+                            }
+                            player.sendMessage(ChatColor.GREEN + "lololololol");
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
