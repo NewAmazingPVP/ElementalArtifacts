5744e586cece46833d336dd7914f39a388ebbb6e
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-24 20:46:40 -0400

    1

--- src/main/java/org/elemental/listeners/Materials_Recipes_Listener.java
+++ src/main/java/org/elemental/listeners/Materials_Recipes_Listener.java
@@ -28,7 +28,29 @@ public class Materials_Recipes_Listener implements Listener {
             if (event.getCurrentItem().getDisplayName().equalsIgnoreCase(ChatColor.BLUE + "" + ChatColor.BOLD + "Mixed Eye" + ChatColor.DARK_AQUA + " [Item]") && event.getCurrentItem().getType() == Material.ENDER_EYE) {
                 Player player = (Player) event.getView().getPlayer();
                 player.playSound(player.getLocation(), "minecraft:block.enchantment_table.use", 1.0f, 2.0f);
-                Inventory Guide = Bukkit.createInventory(player, 27, ChatColor.GOLD + "" + ChatColor.BOLD + "Recipe");
+                Inventory Guide = Bukkit.createInventory(player, 45, ChatColor.GOLD + "" + ChatColor.BOLD + "Recipe");
+
+                ItemStack G1 = new ItemStack(Material.CYAN_STAINED_GLASS_PANE);
+                ItemMeta M1 = G1.getItemMeta();
+                M1.setDisplayName(" ");
+                G1.setItemMeta(M1);
+
+                Guide.setItem(0, G1);
+                Guide.setItem(1, G1);
+                Guide.setItem(2, G1);
+                Guide.setItem(3, G1);
+                Guide.setItem(4, G1);
+                Guide.setItem(5, G1);
+                Guide.setItem(6, G1);
+                Guide.setItem(7, G1);
+                Guide.setItem(8, G1);
+                Guide.setItem(9, G1);
+                Guide.setItem(10, G1);
+                Guide.setItem(11, G1);
+
+                Guide.setItem(15, G1);
+                Guide.setItem(16, G1);
+                Guide.setItem(17, G1);
 
                 player.openInventory(Guide);
             }
