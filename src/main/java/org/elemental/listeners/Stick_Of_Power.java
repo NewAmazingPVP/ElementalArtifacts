c8be95a26c285eebd56a4ef0385e7042cb85c53f
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-27 21:24:52 -0400

    1

--- src/main/java/org/elemental/listeners/Stick_Of_Power.java
+++ src/main/java/org/elemental/listeners/Stick_Of_Power.java
@@ -15,29 +15,15 @@ import org.bukkit.inventory.ItemStack;
 public class Stick_Of_Power implements Listener {
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        if (event.getAction() != Action.RIGHT_CLICK_BLOCK && event.getAction() != Action.RIGHT_CLICK_AIR) {
-            return; // Ignore other click actions
+        if (event.getAction().name().contains("RIGHT_CLICK") && !event.getPlayer().isSneaking()) {
+            if (event.getItem() != null && event.getItem().getType() == Material.AIR) {
+                if (event.getItem().getItemMeta() != null && event.getItem().getItemMeta().hasDisplayName()) {
+                    String itemName = event.getItem().getItemMeta().getDisplayName();
+                    if (itemName.equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Montu's Staff" + ChatColor.DARK_AQUA + " [Wand]")) {
+                        event.getPlayer().sendMessage("Right Click");
+                    }
+                }
+            }
         }
-
-        ItemStack item = event.getItem();
-        if (item == null || item.getType() != Material.AIR) {
-            return; // Ignore if the player is not holding an item
-        }
-
-        if (!item.hasItemMeta() || !item.getItemMeta().hasDisplayName()) {
-            return; // Ignore if the item doesn't have a display name
-        }
-
-        String displayName = item.getItemMeta().getDisplayName();
-        if (!displayName.equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Montu's Staff" + ChatColor.DARK_AQUA + " [Wand]")) {
-            return; // Ignore if the display name is not "111111111111111111111111"
-        }
-
-        if (event.getPlayer().isSneaking()) {
-            return; // Ignore if the player is shifting
-        }
-
-        // Perform action when conditions met
-        event.getPlayer().sendMessage("Right Click");
     }
 }
