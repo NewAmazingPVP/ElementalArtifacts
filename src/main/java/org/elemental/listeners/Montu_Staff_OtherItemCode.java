aeb3c266d49e93ed54df6f88f40777d82778e694
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-28 21:38:00 -0400

    1

--- src/main/java/org/elemental/listeners/Montu_Staff_OtherItemCode.java
+++ src/main/java/org/elemental/listeners/Montu_Staff_OtherItemCode.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -11,8 +12,8 @@ import java.net.http.WebSocket;
 
 public class Montu_Staff_OtherItemCode implements Listener {
     @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        ItemStack item = event.getItemInHand();
+    public void onBlockPlace(PlayerInteractEvent event) {
+        ItemStack item = event.getItem();
         ItemMeta meta = item.getItemMeta();
 
         if (meta != null && meta.hasDisplayName() && meta.getDisplayName().equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Montu's Staff" + ChatColor.DARK_AQUA + " [Wand]")) {
