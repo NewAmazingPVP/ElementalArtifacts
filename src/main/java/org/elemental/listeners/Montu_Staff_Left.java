50a5d04f2c4c08e5548fa39cfd620df7efe86eb9
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-28 21:00:57 -0400

    1

--- src/main/java/org/elemental/listeners/Montu_Staff_Left.java
+++ src/main/java/org/elemental/listeners/Montu_Staff_Left.java
@@ -1,9 +1,6 @@
 package org.elemental.listeners;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Color;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
+import org.bukkit.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -19,13 +16,13 @@ public class Montu_Staff_Left implements Listener {
         ItemStack itemInHand = player.getInventory().getItemInMainHand();
 
         // Check if the player is not crouching
-        if(!player.isSneaking()) {
+        if (!player.isSneaking()) {
             // Check if the player is left-clicking
-            if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
+            if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
                 // Check if the player is holding an item named "11111111111111"
-                if(itemInHand != null && itemInHand.hasItemMeta() && itemInHand.getItemMeta().hasDisplayName()) {
+                if (itemInHand != null && itemInHand.hasItemMeta() && itemInHand.getItemMeta().hasDisplayName()) {
                     String name = itemInHand.getItemMeta().getDisplayName();
-                    if(name.equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Montu's Staff" + ChatColor.DARK_AQUA + " [Wand]")) {
+                    if (name.equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Montu's Staff" + ChatColor.DARK_AQUA + " [Wand]")) {
                         // Send a message to the player with the name of the item
                         player.sendMessage("Left");
                         player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
@@ -33,17 +30,11 @@ public class Montu_Staff_Left implements Listener {
                         player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
                         player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
                         player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
-                        double x = player.getLocation().getX();
-                        double y = player.getLocation().getY();
-                        double z = player.getLocation().getZ();
-                        double dx = player.getLocation().getDirection().getX();
-                        double dy = player.getLocation().getDirection().getY();
-                        double dz = player.getLocation().getDirection().getZ();
-
+                        Location location = event.getPlayer().getEyeLocation();
                         for (int i = 0; i < 7; i++) {
-                            player.spawnParticle(Particle.REDSTONE, x + dx * i, y + dy * i, z + dz * i, 0, 0, 0, 0, new Particle.DustOptions(Color.PURPLE, 1.0f));
+                            location.getWorld().spawnParticle(Particle.REDSTONE, location, 1, 0, 0, 0, 0);
+                            location.add(location.getDirection());
                         }
-
                     }
                 }
             }
