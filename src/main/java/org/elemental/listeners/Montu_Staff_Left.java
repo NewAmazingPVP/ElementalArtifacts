311fab09d60213b1e1b76b48df7c5bc757ddf5ea
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-09-28 21:58:19 -0400

    1

--- src/main/java/org/elemental/listeners/Montu_Staff_Left.java
+++ src/main/java/org/elemental/listeners/Montu_Staff_Left.java
@@ -1,13 +1,24 @@
 package org.elemental.listeners;
 
 import org.bukkit.*;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.block.Action;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.entity.Entity;
 
 
 public class Montu_Staff_Left implements Listener {
@@ -16,21 +27,21 @@ public class Montu_Staff_Left implements Listener {
         Player player = event.getPlayer();
         ItemStack itemInHand = player.getInventory().getItemInMainHand();
 
-        // Check if the player is not crouching
+
         if (!player.isSneaking()) {
-            // Check if the player is left-clicking
+
             if (event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) {
-                // Check if the player is holding an item named "11111111111111"
+
                 if (itemInHand != null && itemInHand.hasItemMeta() && itemInHand.getItemMeta().hasDisplayName()) {
                     String name = itemInHand.getItemMeta().getDisplayName();
                     if (name.equals(ChatColor.GOLD + "" + ChatColor.BOLD + "Montu's Staff" + ChatColor.DARK_AQUA + " [Wand]")) {
-                        // Send a message to the player with the name of the item
+
                         player.sendMessage("Left");
+                        /*player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
                         player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
                         player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
                         player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
-                        player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
-                        player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);
+                        player.playSound(player.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 1.0f, 2.0f);*/
                         player.playSound(player.getLocation(), Sound.BLOCK_BEACON_POWER_SELECT, 1.0f, 2.0f);
                         Location location = player.getEyeLocation().add(0, 0.2, 0);
                         Vector direction = player.getLocation().getDirection();
@@ -38,6 +49,16 @@ public class Montu_Staff_Left implements Listener {
                             location.add(direction);
                             player.getWorld().spawnParticle(Particle.REDSTONE, location, 0, new Particle.DustOptions(Color.PURPLE, 1.0F));
                             player.getWorld().spawnParticle(Particle.REDSTONE, location, 0, new Particle.DustOptions(Color.BLACK, 2.0F));
+                            Vector direction2 = player.getLocation().getDirection();
+                            double range = 15;
+                            for (Entity entity : player.getNearbyEntities(range, range, range)) {
+                                Vector entityVector = entity.getLocation().subtract(player.getLocation()).toVector();
+                                if (entityVector.normalize().equals(direction2.normalize())) {
+                                    /*entity.setLastDamage(2);
+                                    entity.setLastDamager(player);*/
+                                    Event event2 = new EntityDamageByEntityEvent(entity, player, EntityDamageEvent.DamageCause.ENTITY_ATTACK, 2);
+                                    Bukkit.getServer().getPluginManager().callEvent(event2);
+
                         }
                     }
                 }
