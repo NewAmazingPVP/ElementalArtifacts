6998ad35499cbe2a0d86b8ef0e0a5bef08bd0b18
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-10-22 15:26:41 -0400

    1

--- src/main/java/org/elemental/listeners/AntiGuiOpen.java
+++ src/main/java/org/elemental/listeners/AntiGuiOpen.java
@@ -1,7 +1,10 @@
 package org.elemental.listeners;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
@@ -11,10 +14,13 @@ public class AntiGuiOpen implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event){
+        Player player = event.getPlayer();
         if(event.getAction() == Action.RIGHT_CLICK_BLOCK){
             Block block = event.getClickedBlock();
             if(block.getType() == Material.ANVIL){
                 event.setCancelled(true);
+                player.sendMessage(ChatColor.RED + "This is currently disabled!");
+                player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, 2.0f, 0.0f);
             }
         }
     }
