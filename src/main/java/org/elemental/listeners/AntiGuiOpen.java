c74bbd24d885f8e139c0d97d464e5c3afa9d116e
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-10-22 14:54:42 -0400

    1

--- src/main/java/org/elemental/listeners/AntiGuiOpen.java
+++ src/main/java/org/elemental/listeners/AntiGuiOpen.java
@@ -3,10 +3,11 @@ package org.elemental.listeners;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-public class AntiGuiOpen {
+public class AntiGuiOpen implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event){
