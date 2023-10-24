def98e2f2f4327ffdf19f6bed15985da68a81690
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-10-23 21:13:47 -0400

    neds fixing

--- src/main/java/org/elemental/ElementalArtifacts.java
+++ src/main/java/org/elemental/ElementalArtifacts.java
@@ -1,12 +1,21 @@
 package org.elemental;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.Listener;
+import org.bukkit.inventory.ItemFlag;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.elemental.MiningListsners.Loot_Block_Mine;
 import org.elemental.command.GivePlasmaBlockPick_1;
 import org.elemental.listeners.*;
 
 import org.bukkit.plugin.java.JavaPlugin;
 import org.elemental.utility.AutoUpload;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public final class ElementalArtifacts extends JavaPlugin implements Listener {
     public static ElementalArtifacts elementalArtifacts;
@@ -55,6 +64,28 @@ public final class ElementalArtifacts extends JavaPlugin implements Listener {
         getCommand("GivePlasmaPick_1").setExecutor(new GivePlasmaBlockPick_1());
         getServer().getPluginManager().registerEvents(new AntiGuiOpen(), this);
         getServer().getPluginManager().registerEvents(new Loot_Block_Mine(), this);
-    }
+        //-------------------------Item functions -----------------------
+        public static ItemStack PlasmaPic_1 () {
+            ItemStack pick = new ItemStack(Material.WOODEN_PICKAXE);
+            ItemMeta SI = pick.getItemMeta();
+            SI.addEnchant(Enchantment.DURABILITY, 1, false);
+            SI.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+            SI.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "Plasma Block Pickaxe" + ChatColor.LIGHT_PURPLE + " T1");
+            SI.setUnbreakable(true);
+            SI.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+            List<String> BL = new ArrayList<>();
+            BL.add(ChatColor.LIGHT_PURPLE + "Use this to break" + ChatColor.DARK_AQUA + "" + ChatColor.BOLD + " Plasma Blocks");
+            BL.add(" ");
+            BL.add(ChatColor.RED + "Pickaxe is so old that it will");
+            BL.add(ChatColor.RED + "slow your mining when you use it!");
+            BL.add(" ");
+            BL.add(ChatColor.YELLOW + "" + ChatColor.BOLD + "Right Click" + ChatColor.GREEN + " To view T2 recipe!");
+            SI.setLore(BL);
+            SI.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+            pick.setItemMeta(SI);
 
+            return pick;
+        }
+    }
 }
+    
