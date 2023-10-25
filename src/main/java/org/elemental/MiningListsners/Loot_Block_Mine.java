09c7d88f1985f9561f6a6717c46ad3094da767fa
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-10-25 17:51:43 -0400

    1

--- src/main/java/org/elemental/MiningListsners/Loot_Block_Mine.java
+++ src/main/java/org/elemental/MiningListsners/Loot_Block_Mine.java
@@ -18,6 +18,9 @@ import org.bukkit.potion.PotionEffectType;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.elemental.variables.PremadeItems.plasmaenergy;
+import static org.elemental.variables.PremadeItems.plasmascrap;
+
 public class Loot_Block_Mine implements Listener {
 
     @EventHandler
@@ -33,34 +36,14 @@ public class Loot_Block_Mine implements Listener {
                 if (Math.random() < 1.0) {
                     event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.AIR, 0));
 
-                    ItemStack plasmascrap = new ItemStack(Material.LAPIS_LAZULI);
-                    ItemMeta SI = plasmascrap.getItemMeta();
-                    SI.addEnchant(Enchantment.DURABILITY, 1, false);
-                    SI.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-                    SI.setDisplayName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Plasma scrap" + ChatColor.BLUE + "" + ChatColor.BOLD + " [Uncommon]");
-                    SI.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-                    List<String> BL = new ArrayList<>();
-                    BL.add(ChatColor.DARK_GREEN + "Use this to craft and charge up some items!");
-                    SI.setLore(BL);
-                    plasmascrap.setItemMeta(SI);
-
-                    player.getInventory().addItem(plasmascrap);
+                    player.getInventory().addItem(plasmascrap());
 
                 }
 
                 if (Math.random() < 0.05) {
                     event.getBlock().getWorld().dropItemNaturally(event.getBlock().getLocation(), new ItemStack(Material.AIR, 0));
 
-                    ItemStack plasmaenergy = new ItemStack(Material.NETHER_STAR);
-                    ItemMeta SI = plasmaenergy.getItemMeta();
-                    SI.setDisplayName(ChatColor.DARK_AQUA + "" + ChatColor.BOLD + "Plasma energy" + ChatColor.DARK_BLUE + "" + ChatColor.BOLD + " [Rare]");
-                    SI.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-                    List<String> BL = new ArrayList<>();
-                    BL.add(ChatColor.DARK_GREEN + "Used to craft.");
-                    SI.setLore(BL);
-                    plasmaenergy.setItemMeta(SI);
-
-                    player.getInventory().addItem(plasmaenergy);
+                    player.getInventory().addItem(plasmaenergy());
                 }
             }
             if (event.getBlock().getType() != Material.AMETHYST_BLOCK) {
