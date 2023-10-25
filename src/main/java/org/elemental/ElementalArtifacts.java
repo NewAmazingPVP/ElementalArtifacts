b3aa8d362230567e10dff8e7838008b23d213a66
NewAmazingPVP <87737759+NewAmazingPVP@users.noreply.github.com>
2023-10-25 10:53:48 -0400

    Update ElementalArtifacts.java

--- src/main/java/org/elemental/ElementalArtifacts.java
+++ src/main/java/org/elemental/ElementalArtifacts.java
@@ -65,27 +65,7 @@ public final class ElementalArtifacts extends JavaPlugin implements Listener {
         getServer().getPluginManager().registerEvents(new AntiGuiOpen(), this);
         getServer().getPluginManager().registerEvents(new Loot_Block_Mine(), this);
         //-------------------------Item functions -----------------------
-        public static ItemStack PlasmaPic_1 () {
-            ItemStack pick = new ItemStack(Material.WOODEN_PICKAXE);
-            ItemMeta SI = pick.getItemMeta();
-            SI.addEnchant(Enchantment.DURABILITY, 1, false);
-            SI.addItemFlags(ItemFlag.HIDE_ENCHANTS);
-            SI.setDisplayName(ChatColor.AQUA + "" + ChatColor.BOLD + "Plasma Block Pickaxe" + ChatColor.LIGHT_PURPLE + " T1");
-            SI.setUnbreakable(true);
-            SI.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
-            List<String> BL = new ArrayList<>();
-            BL.add(ChatColor.LIGHT_PURPLE + "Use this to break" + ChatColor.DARK_AQUA + "" + ChatColor.BOLD + " Plasma Blocks");
-            BL.add(" ");
-            BL.add(ChatColor.RED + "Pickaxe is so old that it will");
-            BL.add(ChatColor.RED + "slow your mining when you use it!");
-            BL.add(" ");
-            BL.add(ChatColor.YELLOW + "" + ChatColor.BOLD + "Right Click" + ChatColor.GREEN + " To view T2 recipe!");
-            SI.setLore(BL);
-            SI.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
-            pick.setItemMeta(SI);
 
-            return pick;
-        }
     }
 }
     
