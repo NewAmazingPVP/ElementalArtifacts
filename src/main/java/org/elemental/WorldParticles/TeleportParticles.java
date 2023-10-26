5fe4ed46eabaa1768bfab31021849533e0247786
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-10-25 22:11:39 -0400

    1

--- src/main/java/org/elemental/WorldParticles/TeleportParticles.java
+++ src/main/java/org/elemental/WorldParticles/TeleportParticles.java
@@ -1,6 +1,9 @@
 package org.elemental.WorldParticles;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -11,7 +14,10 @@ public class TeleportParticles {
         new BukkitRunnable() {
             @Override
             public void run() {
-                elementalArtifacts.getServer().broadcastMessage("Alr so this works ig every 20 ticks or every second ");
+                Location mapEdgeloc1 = new Location(Bukkit.getWorld("world"), 50.0, -20.0, -120.0);
+                World world = ...;
+                Location location = new Location(world, 100, 100, 100);
+                world.spawnParticle(Particle.FLAME, location, 10);
             }
         }.runTaskTimer(elementalArtifacts, 0, 20);
     }
