6e4a7e867ac7a31a576c3e1772703e9dfe7dbe2f
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-10-26 19:50:30 -0400

    1

--- src/main/java/org/elemental/WorldParticles/TeleportParticles.java
+++ src/main/java/org/elemental/WorldParticles/TeleportParticles.java
@@ -9,15 +9,48 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 import static org.elemental.ElementalArtifacts.elementalArtifacts;
 
+
 public class TeleportParticles {
+    private static final double SPEED = 0.1;
+    private static double y = 0;
+    private static double x = 0;
+    private static double z = 0;
+
+    public static void test(){
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                Location location = new Location(Bukkit.getWorld("world"), x, y, z);
+                location.getWorld().spawnParticle(Particle.VILLAGER_HAPPY, location, 1, 0, 0, 0, 0);
+
+                y += SPEED;
+                x += SPEED;
+                z += SPEED;
+            }
+        }.runTaskTimer(elementalArtifacts, 0, 20);
+    }
+}
+
+
+
+
+
+
+
+/*public class TeleportParticles {
     public static void test(){
         new BukkitRunnable() {
             @Override
             public void run() {
                 Location location = new Location(Bukkit.getWorld("world"), 265.0, 105.0, -28.0);
                 Bukkit.getWorld("world").spawnParticle(Particle.CLOUD, location, 10);
+
+
+
+
+
             }
         }.runTaskTimer(elementalArtifacts, 0, 20);
     }
 
-}
+}*/
