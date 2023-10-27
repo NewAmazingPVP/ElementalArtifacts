096d45053932f004b7e8ad9d53f1acd945577b34
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-10-26 21:37:09 -0400

    1

--- src/main/java/org/elemental/WorldParticles/TeleportParticles.java
+++ src/main/java/org/elemental/WorldParticles/TeleportParticles.java
@@ -18,9 +18,14 @@ public class TeleportParticles {
         new BukkitRunnable() {
             @Override
             public void run() {
-                Location location = new Location(Bukkit.getWorld("world"), 265.0, 105.0, -28.0);
-                Bukkit.getWorld("world").spawnParticle(Particle.COMPOSTER, location, 10);
-
+                Location location1 = new Location(Bukkit.getWorld("world"), 265.0, 105.5, -28.0);
+                Location location2 = new Location(Bukkit.getWorld("world"), 265.0, 105.5, -29.0);
+                Location location3 = new Location(Bukkit.getWorld("world"), 266.0, 105.5, -28.0);
+                Location location4 = new Location(Bukkit.getWorld("world"), 266.0, 105.5, -29.0);
+                Bukkit.getWorld("world").spawnParticle(Particle.COMPOSTER, location1, 10);
+                Bukkit.getWorld("world").spawnParticle(Particle.COMPOSTER, location2, 10);
+                Bukkit.getWorld("world").spawnParticle(Particle.COMPOSTER, location3, 10);
+                Bukkit.getWorld("world").spawnParticle(Particle.COMPOSTER, location4, 10);
 
 
             }
