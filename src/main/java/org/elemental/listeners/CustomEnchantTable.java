7a16d57814dd59294749c19f7835e998d08a6bee
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-10-28 11:16:22 -0400

    1

--- src/main/java/org/elemental/listeners/CustomEnchantTable.java
+++ src/main/java/org/elemental/listeners/CustomEnchantTable.java
@@ -1,5 +1,6 @@
 package org.elemental.listeners;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.Sound;
@@ -9,6 +10,9 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class CustomEnchantTable implements Listener {
 
@@ -20,6 +24,12 @@ public class CustomEnchantTable implements Listener {
             if(block.getType() == Material.ENCHANTING_TABLE){
                 event.setCancelled(true);
                 player.playSound(player.getLocation(), Sound.BLOCK_PORTAL_TRIGGER, 2.0f, 2.0f);
+                Inventory alter = Bukkit.createInventory(player, 54, ChatColor.LIGHT_PURPLE + "" + ChatColor.BOLD + "Ability Altar");
+
+                ItemStack G1 = new ItemStack(Material.PURPLE_STAINED_GLASS_PANE);
+                ItemMeta M1 = G1.getItemMeta();
+                M1.setDisplayName(" ");
+                G1.setItemMeta(M1);
             }
         }
     }
