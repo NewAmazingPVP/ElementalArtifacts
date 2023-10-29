d87b731d80b32d2c7f39093bc6fb215714a4235b
Comet991 <139285587+Comet991@users.noreply.github.com>
2023-10-28 22:22:35 -0400

    1

--- src/main/java/org/elemental/variables/PremadeItems.java
+++ src/main/java/org/elemental/variables/PremadeItems.java
@@ -1,11 +1,13 @@
 package org.elemental.variables;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.LeatherArmorMeta;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -50,7 +52,7 @@ public class PremadeItems {
         return plasmascrap;
     }
 
-    public static ItemStack plasmaenergy(){
+    public static ItemStack plasmaenergy() {
 
         ItemStack plasmaenergy = new ItemStack(Material.AMETHYST_SHARD);
         ItemMeta SI = plasmaenergy.getItemMeta();
@@ -64,7 +66,7 @@ public class PremadeItems {
         return plasmaenergy;
     }
 
-    public static ItemStack starDustChisel_1(){
+    public static ItemStack starDustChisel_1() {
 
         ItemStack starDustChisel_1 = new ItemStack(Material.ARROW);
         ItemMeta SI = starDustChisel_1.getItemMeta();
@@ -82,7 +84,7 @@ public class PremadeItems {
         return starDustChisel_1;
     }
 
-    public static ItemStack starDust(){
+    public static ItemStack starDust() {
 
         ItemStack starDust = new ItemStack(Material.NETHER_STAR);
         ItemMeta SI = starDust.getItemMeta();
@@ -96,6 +98,52 @@ public class PremadeItems {
         return starDust;
     }
 
+    public static ItemStack StarDustMineArmor() {
 
+        ItemStack StarDustMineChest = new ItemStack(Material.LEATHER_CHESTPLATE);
+        LeatherArmorMeta chest = (LeatherArmorMeta) StarDustMineChest.getItemMeta();
+        chest.setColor(Color.BLUE);
+        chest.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + "Star Dust Mines Chestplate");
+        chest.setUnbreakable(true);
+        chest.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        chest.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        List<String> SCL = new ArrayList<>();
+        SCL.add(" ");
+        SCL.add(ChatColor.DARK_GREEN + "Low level armor used in the mines.");
+        SCL.add(" ");
+        chest.setLore(SCL);
+        StarDustMineChest.setItemMeta(chest);
+
+        ItemStack StarDustMineLegs = new ItemStack(Material.LEATHER_LEGGINGS);
+        LeatherArmorMeta legs = (LeatherArmorMeta) StarDustMineLegs.getItemMeta();
+        legs.setColor(Color.BLUE);
+        legs.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + "Star Dust Mines Leggings");
+        legs.setUnbreakable(true);
+        legs.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        legs.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        List<String> SLL = new ArrayList<>();
+        SLL.add(" ");
+        SLL.add(ChatColor.DARK_GREEN + "Low level armor used in the mines.");
+        SLL.add(" ");
+        legs.setLore(SLL);
+        StarDustMineLegs.setItemMeta(legs);
+
+        ItemStack StarDustMineBoots = new ItemStack(Material.LEATHER_BOOTS);
+        LeatherArmorMeta boots = (LeatherArmorMeta) StarDustMineBoots.getItemMeta();
+        boots.setColor(Color.BLUE);
+        boots.setDisplayName(ChatColor.BLUE + "" + ChatColor.BOLD + "Star Dust Mines Boots");
+        boots.setUnbreakable(true);
+        boots.addItemFlags(ItemFlag.HIDE_UNBREAKABLE);
+        boots.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+        List<String> SBL = new ArrayList<>();
+        SBL.add(" ");
+        SBL.add(ChatColor.DARK_GREEN + "Low level armor used in the mines.");
+        SBL.add(" ");
+        boots.setLore(SLL);
+        StarDustMineBoots.setItemMeta(boots);
+
+
+        return StarDustMineChest;
+    }
 }
 
